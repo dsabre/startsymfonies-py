dc838c3fbb69236fd774aad280bf34e31cefef3a
Daniele Sabre <daniele.sabre@gmail.com>
2016-10-11 17:17:12 +0200

    Addded column status (see #1) and permitted to skip directories (see #2)

--- startsymfonies.py
+++ startsymfonies.py
@@ -15,8 +15,9 @@ if not os.path.isfile(configFile):
     config = open(configFile, 'w')
     Config.add_section('config')
     Config.set('config', 'dir', 'INSER_HERE_YOUR_PATH')
-    Config.set('config', 'htmlFilename', '/tmp/symfonies.html')
-    Config.set('config', 'htmlTitle', 'Active Symfonies')
+    Config.set('config', 'skipdirs', '')
+    Config.set('config', 'htmlfilename', '/tmp/symfonies.html')
+    Config.set('config', 'htmltitle', 'Active Symfonies')
     Config.write(config)
     config.close()
     print 'Config file not found, no problem, we have now created that for you! ;)\nOpen ' + configFile + ' and set your directory to scan.'
@@ -48,21 +49,32 @@ localIp = "127.0.0."
 finalLocalIp = 1
 publicPort = 8000
 symfonies = []
-htmlFilename = Config.get('config', 'htmlfilename')
-htmlTitle = Config.get('config', 'htmltitle')
+htmlfilename = Config.get('config', 'htmlfilename')
+htmltitle = Config.get('config', 'htmltitle')
+
+# get dirs to skip
+skipdirs = Config.get('config', 'skipdirs')
+skipdirs = skipdirs.split(',') if skipdirs else None
 
 # cycle over directory for find all symfonies
 for dirname, dirnames, filenames in os.walk(dir):
     fname = dirname + '/app/console'
     if os.path.isfile(fname):
-        symfonies.append(dirname)
+        skip = False
+
+        if skipdirs:
+            for i in skipdirs:
+                if dirname.find(i) > -1:
+                    skip = True
+
+        symfonies.append({'dirname': dirname, 'skip': skip})
 
 if symfonies:
     # order symfonies by dirname
-    symfonies = sorted(symfonies)
+    symfonies = sorted(symfonies, key=lambda symfony: symfony['dirname'])
 
     # creation of menu html file
-    target = open(htmlFilename, 'w')
+    target = open(htmlfilename, 'w')
 
     target.writelines([
         '<!DOCTYPE html>\n',
@@ -71,17 +83,18 @@ if symfonies:
         '\t<meta charset="utf-8">\n',
         '\t<meta http-equiv="X-UA-Compatible" content="IE=edge">\n',
         '\t<meta name="viewport" content="width=device-width, initial-scale=1">\n',
-        '\t<title>' + htmlTitle + '</title>\n',
+        '\t<title>' + htmltitle + '</title>\n',
         '\t<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">\n',
         '</head>\n',
         '<body>\n',
         '\t<div class="container-fluid">\n',
-        '\t<h1>' + htmlTitle + '</h1>\n',
+        '\t<h1>' + htmltitle + '</h1>\n',
         '\t<div class="table-responsive">\n',
         '\t\t<table class="table table-bordered table-hover">\n',
         '\t\t\t<tr>\n',
         '\t\t\t\t<th>Favicon</th>\n',
         '\t\t\t\t<th>Path</th>\n',
+        '\t\t\t\t<th>Status</th>\n',
         '\t\t\t\t<th>Private link</th>\n',
         '\t\t\t\t<th>Public link</th>\n',
         '\t\t\t</tr>\n'
@@ -90,56 +103,86 @@ if symfonies:
     # cycle over directory for find all symfonies
     i = 1
     count = str(len(symfonies))
-    for symfony in symfonies:
-        # define local address
-        address = localIp + str(finalLocalIp)
+    for infoSymfony in symfonies:
+        symfony = infoSymfony['dirname']
 
-        fname = symfony + '/app/console'
+        stopped = False
+        started = False
+        skipped = infoSymfony['skip']
+        privateAddress = ''
+        publicAddress = ''
 
         print str(i) + '/' + count + ' ::: ' + symfony
         i += 1
 
-        if not startOnly:
-            sys.stdout.write('STOP : ')
+        if not skipped:
+            # define local address
+            address = localIp + str(finalLocalIp)
 
-            # try to stop private and public symfony
-            s1 = call(["php", fname, "-q", "server:stop", address])
-            s2 = call(["php", fname, "-q", "server:stop", publicIp, "-p", str(publicPort)])
+            fname = symfony + '/app/console'
 
+            if not startOnly:
+                sys.stdout.write('STOP : ')
+
+                # try to stop private and public symfony
+                s1 = call(["php", fname, "-q", "server:stop", address])
+                s2 = call(["php", fname, "-q", "server:stop", publicIp, "-p", str(publicPort)])
+
+                # print operation status
+                if s1 == 0 and s2 == 0:
+                    print 'OK'
+                    stopped = True
+                else:
+                    print 'KO'
+
+                sys.stdout.write('START: ')
+
+                sleep(1)
+            else:
+                sys.stdout.write('START: ')
+
+            # try to start private and public symfony
+            s1 = call(["php", fname, "-q", "server:start", address])
+            s2 = call(["php", fname, "-q", "server:start", publicIp, "-p", str(publicPort)])
+
+            # print operation status
             if s1 == 0 and s2 == 0:
                 print 'OK'
+                started = True
             else:
                 print 'KO'
 
-            sys.stdout.write('START: ')
+            privateAddress = 'http://' + address + ':8000'
+            publicAddress = 'http://' + publicIp + ':' + str(publicPort)
 
-            sleep(1)
+            finalLocalIp += 1
+            publicPort += 1
         else:
-            sys.stdout.write('START: ')
+            print 'SKIPPED'
 
-        # try to start private and public symfony
-        s1 = call(["php", fname, "-q", "server:start", address])
-        s2 = call(["php", fname, "-q", "server:start", publicIp, "-p", str(publicPort)])
-
-        if s1 == 0 and s2 == 0:
-            print 'OK'
+        # get the status of the symfony
+        if started:
+            status = 'Active'
+            bgClass = 'success'
+        elif stopped:
+            status = 'Stopped'
+            bgClass = 'danger'
+        elif skipped:
+            status = 'Skipped'
+            bgClass = 'info'
         else:
-            print 'KO'
-
-        privateAddress = 'http://' + address + ':8000'
-        publicAddress = 'http://' + publicIp + ':' + str(publicPort)
+            status = '--'
+            bgClass = 'warning'
 
         target.write('\t\t\t<tr>\n')
         target.write('\t\t\t\t<td class="text-center"><img src="' + symfony + '/web/favicon.ico' + '" alt="No favicon" width="16" /></td>\n')
         target.write('\t\t\t\t<td>' + symfony + '</td>\n')
+        target.write('\t\t\t\t<td class="bg-' + bgClass + '">' + status + '</td>\n')
         target.write('\t\t\t\t<td><a href="' + privateAddress + '">' + privateAddress + '</a></td>\n')
         target.write('\t\t\t\t<td><a href="' + publicAddress + '">' + publicAddress + '</a></td>\n')
         target.write('\t\t\t</tr>\n')
 
         print ''
-
-        finalLocalIp += 1
-        publicPort += 1
 
     target.writelines([
         '\t\t</table>\n',
@@ -155,6 +198,6 @@ if symfonies:
     target.close()
 
     # launch default browser with html menu file
-    call(['gnome-open', "file://" + htmlFilename])
+    call(['gnome-open', "file://" + htmlfilename])
 
 sys.exit(0)
